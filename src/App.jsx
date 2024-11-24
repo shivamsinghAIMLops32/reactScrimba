26aeb571eadb85d2a2339e1e704ae52a1335b936
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2024-11-24 15:18:02 +0530

    scrimba react course

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const App = ()=>{
+  return (
+     <div className='header'>hey world bye 
+      <div> <img src="/src/assets/react.svg" alt="viteLogo" /></div>
+     </div>
+  )
+}
+
+export default App;
